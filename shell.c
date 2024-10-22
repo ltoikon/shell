7d7ef1558255094d928b5589e0fa12f7bcae7ec1
Lauri Ikonen <lto.ikonen@gmail.com>
2024-10-22 17:22:21 +0300

    linked list added

--- shell.c
+++ shell.c
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-/*Linked list used for saving paths*/
+/*Linked list data structure is used for saving paths*/
 typedef struct List {
     char *textData;
     struct List* pNext;
@@ -73,28 +73,15 @@ List * removeList(List *pRoot){
     return pRoot;
 };
 
-int writeList(List *pRoot, char *filename){
+int printList(List *pRoot){
     List *ptr = pRoot;
 
-    if (strcmp("stdout", filename) != 0){
-        FILE *pFile;
-        if ((pFile = fopen(filename, "w")) == NULL){
-            fprintf(stderr, "error: cannot open file '%s'\n", filename);
-            exit(1);
-        }
-        while (ptr != NULL){
-            fprintf(pFile, "%s", ptr->textData);
-            ptr = ptr->pNext;
-        }
-        fclose(pFile);
-    }
-    else {
-        //printf("Output:\n");
-        while (ptr != NULL){
-        fprintf(stdout, "%s", ptr->textData);
+    printf("Paths:\n");
+    while (ptr != NULL){
+        fprintf(stdout, "%s\n", ptr->textData);
         ptr = ptr->pNext;
-        }
     }
+    
     return 0;
 }
 
@@ -108,33 +95,48 @@ List * initiatePath(List *pPath){
 
 
 }
-/*TODO: 
- - new path replaces old paths, so need to edit this
- - should i changes how path is saved to array*/
 
 List * updatePath(List *pPath, char *addition){
-    /*
-    char *newPath;
-    size_t newSize = strlen(path) + strlen(addition) + 2;
-    printf("in update function, next reallocation\n");
-    //memory reallocation
-    newPath = realloc(path, newSize);
-    if (newPath == NULL){
-        perror("realloc failed!");
-    }
-    printf("reallocation is done\n");
-    newPath = strncat(newPath,":",2);
-    newPath = strncat(newPath,addition,strlen(addition));
-    printf("%s\n",newPath);
-
-    return newPath;
-    */
+    char * pArgument = strtok(addition, " ");
+  
     // remove old path
+    pPath = removeList(pPath);
     
-    removeList(pPath);
-
+    while(pArgument != NULL){
+        printf("%s\n", pArgument);
+        pPath = additionList(pArgument, pPath);
+        pArgument = strtok(NULL, " ");
+    }
+    
 
     return pPath;
+}
+int changeDirectory(char * parameter){ 
+    //printf("built-in cd\n");
+    if (chdir(parameter) != 0){
+        perror("cd failed!");
+    };
+    return 0;
+}
+
+int execution(char * command, char * argument, List *pPath){
+
+    //TODO
+        // käy läpi jokainen path etsien accessilla commandia
+        // kun löytyy aja command
+        // jos ei löydy pathista ei ajoa
+    pid_t pid = fork();
+    if (pid < 0){
+        perror("Fork failed!");
+        exit(1);
+    }
+    if (pid == 0){
+        char *args[]={command, argument, NULL};
+        execvp(args[0],args);
+    } else {
+        wait(NULL);
+    }
+    return 0;
 }
 
 int main(){
@@ -142,20 +144,19 @@ int main(){
     char *token = NULL;
     char *parameter = NULL;
     List *pPath = NULL;
-    //int argumentsCount = NULL;
     size_t len = 0;
     //__ssize_t read;
 
     //initiate path    
     pPath = initiatePath(pPath);
-    //printf("%s\n",path);
+    printList(pPath);
     while (1){
         
         printf("shell>>");
         getline(&line, &len, stdin);
         token = strtok(line, " ");
-        parameter = strtok(NULL, " ");
-        parameter = strtok(parameter, "\n"); //remove line break from parameter
+        parameter = strtok(NULL, "\n");
+        //parameter = strtok(parameter, "\n"); //remove line break from parameter
         printf("command is %s, and given parameter if given: %s\n", token, parameter);
 
         /*remove linebreak only if string is not empty*/
@@ -169,42 +170,27 @@ int main(){
         }
 
         if (strcmp("vaihto", token) == 0){
-            printf("built-in cd\n");
-            if (chdir(parameter) != 0){
-                perror("cd failed!");
-            };
+            changeDirectory(parameter);
             continue;
         }
 
         if (strcmp("path", token) == 0){
-            printf("built-in path\n");
+            //printf("built-in path\n");
 
-            if (parameter != NULL) {
-                updatePath(path, parameter);
-            } else {
-                printf("No parameter provided for path update\n");
-            }
-            //path = updatePath(path, parameter);
+            pPath = updatePath(pPath, parameter);
+
+            printList(pPath);
             continue;
         }
 
 
         if (strcmp("exit", token) == 0){
-            free(path);
+            pPath = removeList(pPath);
             //path[] = NULL; //How to make sure that path pointer is made to 
             exit(0);
         } else {
-            pid_t pid = fork();
-            if (pid < 0){
-                perror("Fork failed!");
-                exit(1);
-            }
-            if (pid == 0){
-                char *args[]={token, parameter, NULL};
-                execvp(args[0],args);
-            } else {
-                wait(NULL);
-            }
+            execution(token, parameter, pPath);
+            
         }
         
     }
